1c7c81d4aad462b87a8ebf6fdda52265d053acbf
jvan <jvan@cse.ucdavis.edu>
2014-05-22 15:28:28 -0700

    Cleaned up example code.

--- public/examples/cubes.js
+++ public/examples/cubes.js
@@ -2,19 +2,22 @@ function init() {
    background(0.2, 0.2, 0.2);
 
    globals.num_cubes  = 250;
-   globals.positions = random_positions(globals.num_cubes, -15.0, 15.0);
-   globals.colors = random_colors(globals.num_cubes);
-   globals.velocities = random_values(globals.num_cubes, 0.05, 0.1);
+   globals.cubes = [];
+
+   for (var i=0; i<globals.num_cubes; i++) {
+      globals.cubes.push({
+         pos: random_position(-15, 15),
+         col: random_color(),
+         vel: rand(0.05, 0.1)
+      });
+   }
 }
 
 function frame() {
-   for (var i=0; i<globals.num_cubes; i++) {
-      globals.positions[i][0] += globals.velocities[i];
-      if (globals.positions[i][0] > 15.0) {
-         globals.positions[i] = random_position(-15.0, 15.0);
-         globals.positions[i][0] = -15.0;
-      }
-   }
+   globals.cubes.forEach(function(block) {
+      block.pos[0] += block.vel;
+      block.pos[0] = block.pos[0] > 15.0 ? -15.0 : block.pos[0];
+   });
 }
 
 function display() {
@@ -22,16 +25,11 @@ function display() {
 
    scale(0.25, 0.25, 0.25);
 
-   for (var i=0; i<globals.num_cubes; i++) {
-      var t = globals.positions[i];
-      var c = globals.colors[i];
-
-      translate(t[0], t[1], t[2]);
-      color(c[0], c[1], c[2]);
-
+   globals.cubes.forEach(function(block) {
+      translate(block.pos);
+      color(block.col);
       cube('solid');
-
-      translate(-t[0], -t[1], -t[2]);
-   }
+      translate(block.pos.map(function(x) { return -x; }));
+   });
 }
 
